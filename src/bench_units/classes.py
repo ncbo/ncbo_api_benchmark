134e11731e37c569ebc18e53952f0f639b5edebd
msalvadores <msalvadores@gmail.com>
2013-08-15 19:15:26 -0700

    class bench unit

--- src/bench_units/classes.py
+++ src/bench_units/classes.py
@@ -1,60 +1,66 @@
-import mechanize
 import pdb
 import time
-import rest
+import api
+import sys
+import os
+import ezbench
 import json
+import random
 
-APIKEY = ""
-REST_EPR = "ncbo-stg-app-15:80"
-REST_EPR = "localhost:9393"
+class ClassesBenchmark(object):
 
-class Transaction(object):
+    def __init__(self,client):
+        self.client = client
 
-    def __init__(self):
-        self.api = rest.Rest(REST_EPR)
-        self.api.key = APIKEY
-        self.ontology = "NDFRT"
-        self.pageno = 1
-        self.size = 500
-        self.roots = None
+    def run(self,use_onts=None):
+        if use_onts == None:
+            use_onts = ["BRO","NIFSTD","ICPC2P","BIOMODELS","PHARE","HINO","NDDF","VO"]
+        for acronym in use_onts:
+            ont_data = self.client.get_ontology(acronym)
+            roots = self.client.get_roots(acronym)
+            roots = json.loads(roots)
+            classes = []
+            page = self.client.get_classes(acronym)
+            page = json.loads(page)
+            classes.extend(page)
+            all_pages = range(2,page["pageCount"]+1)
+            random.shuffle(all_pages)
+            count = 0
+            while count < 3 && len(all_pages) > 0:
+                pagen = all_pages.pop(0)
+                page = self.client.get_classes(acronym,page=pagen)
+                page = json.loads(page)
+                classes.extend(page)
+                count += 1
+            random.shuffle(classes)
+            count = 0
+            while count < 200 && len(classes) > 0:
+                cls = classes.pop(0)
+                
 
-
-    def sample(self,group,action):
-        if group not in self.custom_timers:
-            self.custom_timers[group]=[] 
-        st = time.time()
-        res = action()
-        et = time.time()
-        self.custom_timers[group].append((st,et))
-        intl_times = self.api.last_internal_times()
-        for (group,trace) in intl_times:
-            if group not in self.custom_timers:
-                self.custom_timers[group]=[] 
-            #self.custom_timers[group].append((st,st+trace))
-        return res
-
-    def run(self):
-        #run clean timers
-        self.start_time = time.time()
-        self.custom_timers = dict()
-        try:
-            if not self.roots:
-                roots_response = self.sample('roots',
-                    lambda: self.api.get_roots(
-                        self.ontology,include="prefLabel,definition,synonym,childrenCount"))
-                self.roots = json.loads(roots_response)
-            classes_response = self.sample('classes_page',
-                    lambda: self.api.get_classes(self.ontology,page=self.pageno,size=self.size))
-            classes = json.loads(classes_response)
-            if classes['nextPage']:
-                self.pageno = classes['nextPage']
-            else:
-                self.pageno = 1
-        except Exception, exc:
-            print exc
 
 if __name__ == '__main__':
-    trans = Transaction()
-    trans.run()
-    print trans.custom_timers
+    epr = sys.argv[1]
+    print "Using %s ..."%epr
+    benchmark = ezbench.Benchmark()
 
+    def query_debug(api):
+        return api.last_query_info()
+    def request_path(api):
+        return api.last_request_path
+
+    benchmark.link(api.Rest.get_classes,subgroups=query_debug,data=request_path)
+    benchmark.link(api.Rest.get_roots,subgroups=query_debug,data=request_path)
+    benchmark.link(api.Rest.get_tree,subgroups=query_debug,data=request_path)
+    benchmark.link(api.Rest.get_children,subgroups=query_debug,data=request_path)
+    benchmark.link(api.Rest.get_descendants,subgroups=query_debug,data=request_path)
+    benchmark.link(api.Rest.get_ancestors,subgroups=query_debug,data=request_path)
+    benchmark.link(api.Rest.get_par,subgroups=query_debug,data=request_path)
+    api_key = os.environ["NCBO_API_KEY"]
+    client = api.Rest(epr,key=api_key)
+    clsb = ClassesBenchmark(client)
+    clsb.run(use_onts=["BRO"])
+    ezbench.report.show(benchmark)
+    fout = os.path.join("results",
+              "bench_" + os.path.basename(__file__) + time.strftime("_%Y%m%d_%H%M_%S.csv"))
+    benchmark.save(fout)
