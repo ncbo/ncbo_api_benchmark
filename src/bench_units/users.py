65ac769cf43a50d255128c0f8062fd4e341b0e1c
msalvadores <msalvadores@gmail.com>
2013-08-15 17:59:24 -0700

    user using show report

--- src/bench_units/users.py
+++ src/bench_units/users.py
@@ -36,19 +36,11 @@ if __name__ == '__main__':
 
     api_key = os.environ["NCBO_API_KEY"]
     client = api.Rest(epr,key=api_key)
-    perc_points = [60,90]
     ub = UsersBenchmark(client)
     for x in range(10):
         print "running iteration %s"%x
         ub.run(n=12)
-    percs = benchmark.percentiles(include=perc_points)
-    print "All groups median (%.3f) worst (%.3f)"%(benchmark.median()["total"],benchmark.maximum()["total"])
-    ezbench.report.print_percentiles(percs,out=sys.stdout)
-    for g in benchmark.groups():
-        print "Group: %s median (%.3f) worst (%.3f)"\
-                %(g,benchmark.median(group=g)["total"],benchmark.maximum(group=g)["total"])
-        percs = benchmark.percentiles(group=g,include=perc_points)
-        ezbench.report.print_percentiles(percs,out=sys.stdout)
+    ezbench.report.show(benchmark)
     fout = os.path.join("results",
               "bench_" + os.path.basename(__file__) + time.strftime("_%Y%m%d_%H%M_%S.csv"))
     benchmark.save(fout)
