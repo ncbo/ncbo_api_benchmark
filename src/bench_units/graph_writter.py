c9bda24f1a35d6235257020b4e697896a246aa3c
msalvadores <msalvadores@gmail.com>
2013-08-30 17:36:17 -0700

    graph writter benchmark

---
+++ src/bench_units/graph_writter.py
@@ -0,0 +1,38 @@
+import pdb
+import time
+import api
+import sys
+import os
+import ezbench
+import json
+import random
+
+class GraphWritterBenchmark(object):
+
+    def __init__(self,client):
+        self.client = client
+
+    def run(self):
+        graph = "http://data.bioportal.org/benchmark/graph/go"
+        with file("./data/go.owl","r") as data:
+            triples = data.read()
+            self.client.append_triples(triples,graph,"application/rdf+xml")
+        #"""SELECT (count(?s) as ?c) WHERE { GRAPH <http://data.bioportal.org/benchmark/graph/go> { ?s a ?o }}"""
+        self.client.delete_graph(graph)
+
+if __name__ == '__main__':
+    epr = sys.argv[1]
+    print "Using SPARQL %s ..."%epr
+
+    benchmark = ezbench.Benchmark()
+    benchmark.link(api.SPARQL.delete_graph)
+    benchmark.link(api.SPARQL.append_triples)
+    benchmark.link(api.SPARQL.update)
+
+    client = api.SPARQL(epr)
+    gw = GraphWritterBenchmark(client)
+    gw.run()
+    ezbench.report.show(benchmark)
+    fout = os.path.join("results",
+              "bench_" + os.path.basename(__file__) + time.strftime("_%Y%m%d_%H%M_%S.csv"))
+    benchmark.save(fout)
