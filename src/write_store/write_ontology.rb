a7f7ffa12f2171c96d64c58c79455ff9512ad3ec
msalvadores <msalvadores@gmail.com>
2013-09-11 19:23:53 -0700

    deafult namespace

--- src/write_store/write_ontology.rb
+++ src/write_store/write_ontology.rb
@@ -17,7 +17,7 @@ Goo.configure do |conf|
   conf.add_redis_backend(:host => "localhost")
   conf.add_namespace(:owl, RDF::Vocabulary.new("http://www.w3.org/2002/07/owl#"))
   conf.add_namespace(:rdfs, RDF::Vocabulary.new("http://www.w3.org/2000/01/rdf-schema#"))
-  conf.add_namespace(:foaf, RDF::Vocabulary.new("http://xmlns.com/foaf/0.1/"))
+  conf.add_namespace(:foaf, RDF::Vocabulary.new("http://xmlns.com/foaf/0.1/"),default=true)
   conf.add_namespace(:rdf, RDF::Vocabulary.new("http://www.w3.org/1999/02/22-rdf-syntax-ns#"))
 end
 
